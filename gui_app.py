f97f8131efc7f181eadb965949fe4d3c0892b8e2
Prajwalnayak2506 <pjnayaks@gmail.com>
2025-08-06 00:59:27 +0530

    Reconnected repo after accidentally removing .git

---
+++ gui_app.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout  # GUI components
+import subprocess #this module to to let this file run another file in the background and then use its output for modifying things in this file
+
+class BlinkApp(QWidget):
+    def __init__(self):
+        super().__init__() 
+
+        self.setWindowTitle("Blink Counter")
+        self.setGeometry(100, 100, 300, 100)# x, y, width, height
+        self.label = QLabel("Blink Count: 0", self) 
+        self.label.setStyleSheet("font-size: 24px;")
+        layout = QVBoxLayout()#Vertical layout (top to bottom)
+        layout.addWidget(self.label) 
+        self.setLayout(layout)
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)  
+    result = subprocess.Popen(["python", "eye-tracker-share\eye_blink_counter.py"],stdout=subprocess.PIPE,stderr=subprocess.PIPE,text=True,bufsize=1)#bufsize=1 meaning one line once
+    window = BlinkApp()
+    window.show()                
+    sys.exit(app.exec_())
